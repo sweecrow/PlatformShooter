bc35151a8622d0f55f15b89120ab1a6fec1d5ce9
sweecrow <william.alfonsson@hotmail.com>
2018-04-05 15:34:14 +0200

    SpelarMotor
    Jag skapade ett script PlayerMotor, som kontrollerar allt om spelaren. Har lagt till så att spelaren kan röra sig med A och D samt hoppa med Space.

---
+++ PlatformShooter/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rigidbody2d;
+
+    public Transform groundCheckPoint;
+    public float GroundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    void Start ()
+    {
+
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+	void Update ()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, GroundCheckRadius, whatIsGround);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2d.velocity = new Vector2(moveSpeed, rigidbody2d.velocity.y);
+        }
+        else
+        {
+            rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
+        }
+
+        if (rigidbody2d.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (rigidbody2d.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
