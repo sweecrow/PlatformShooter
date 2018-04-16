dd53ba53cf64caf8b885054b74be817294b389e6
sweecrow <william.alfonsson@hotmail.com>
2018-04-16 20:22:45 +0200

    Fiende statestik
    Skapade ett script för att hantera fienden. Nu har den liv som kan skadas med en funktion precis som spelaren.

---
+++ PlatformShooter/Assets/Scripts/EnemyMotor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMotor : MonoBehaviour {
+
+    public int healthMax = 30;
+    public int healthCurrent;
+
+    void Awake()
+    {
+        healthCurrent = healthMax;
+    }
+
+    void Update()
+    {
+        if (healthCurrent <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void TakeEnemeDamage()
+    {
+        healthCurrent -= 10;
+    }
+}
