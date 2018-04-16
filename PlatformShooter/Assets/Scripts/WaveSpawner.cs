396d635a17f0ffbbcb72c3c5002e98bd88ac4b8a
sweecrow <william.alfonsson@hotmail.com>
2018-04-16 21:07:24 +0200

    Wave Spawnare
    Skapade ett script för att wave spawna fiender.  Startade smått med scriptet genom att skapa en class för att hantera de olika wavsen inne i Unity.

---
+++ PlatformShooter/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+}
