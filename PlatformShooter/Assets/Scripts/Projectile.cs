17d01157ad2bb3834d756af32f49d3fb22bc1475
sweecrow <william.alfonsson@hotmail.com>
2018-04-16 20:34:26 +0200

    Spelar skada
    Nu skadar även spelaren skott fienden.

--- PlatformShooter/Assets/Scripts/Projectile.cs
+++ PlatformShooter/Assets/Scripts/Projectile.cs
@@ -5,6 +5,11 @@ public class Projectile : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "Enemy1")
+        {
+            FindObjectOfType<EnemyMotor>().TakeEnemeDamage();
+        }
+
         Destroy(gameObject);
     }
 }
