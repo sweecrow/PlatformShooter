0fa7b8d0fdd20ad86b5020089ab59fc44596bbec
william alfonsson <william.alfonsson@hotmail.com>
2018-04-23 19:29:22 +0200

    kamera fix

--- PlatformShooter/Assets/CameraController.cs
+++ PlatformShooter/Assets/CameraController.cs
@@ -3,13 +3,49 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public bool maintainWidth = true;
+    [Range(-1, 1)]
+    public int adaptPosition;
 
-	}
 
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    float defaultWidth;
+    float defaultHeight;
+
+
+    Vector3 CameraPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        CameraPos = Camera.main.transform.position;
+
+        defaultHeight = Camera.main.orthographicSize;
+        defaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (maintainWidth)
+        {
+
+            Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+
+
+            //CameraPos.y was added in case camera in case camera's y is not in 0
+            Camera.main.transform.position = new Vector3(CameraPos.x, CameraPos.y + adaptPosition * (defaultHeight - Camera.main.orthographicSize), CameraPos.z);
+
+
+        }
+        else
+        {
+            //CameraPos.x was added in case camera in case camera's x is not in 0
+            Camera.main.transform.position = new Vector3(CameraPos.x + adaptPosition * (defaultWidth - Camera.main.orthographicSize * Camera.main.aspect), CameraPos.y, CameraPos.z);
+
+        }
+
+
+    }
 }
