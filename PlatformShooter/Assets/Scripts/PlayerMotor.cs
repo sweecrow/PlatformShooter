ae9717b65db35d242266924eb5decc8e423281a5
william alfonsson <william.alfonsson@hotmail.com>
2018-05-18 13:07:57 +0200

    Jobbar på rörelser i multiplayer

--- PlatformShooter/Assets/Scripts/PlayerMotor.cs
+++ PlatformShooter/Assets/Scripts/PlayerMotor.cs
@@ -7,33 +7,21 @@ public class PlayerMotor : NetworkBehaviour
 {
     
     public float moveSpeed;
-    
     public float jumpForce;
     
     public float healthMax = 100;
-   
     public float healthCurrent;
-    
     public Image currentHealthBar;
-    
     public Text ratiotext;
-    
     public float ratio;
-  
     public bool isRegenHealthTrue;
-    
     public int healthReg;
-   
     public Text healthText;
  
     private Rigidbody2D rigidbody2d;
-
     public Transform groundCheckPoint;
-
     public float GroundCheckRadius;
-
     public LayerMask whatIsGround;
-
     public bool isGrounded;
  
     private Animator anim;
@@ -61,36 +49,48 @@ public class PlayerMotor : NetworkBehaviour
 
 	void Update ()
     {
-        if (!isLocalPlayer)
+        if (isLocalPlayer == true)
         {
+            if(Input.GetKey(KeyCode.D))
+            {
+                this.transform.Translate(Vector2.right * Time.deltaTime * 3f);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                this.transform.Translate(Vector2.left * Time.deltaTime * 3f);
+            }
+
+
+            /*
+            isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, GroundCheckRadius, whatIsGround);
             
-            return;
+            if (Input.GetKey(KeyCode.A))
+            {
+                rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                rigidbody2d.velocity = new Vector2(moveSpeed, rigidbody2d.velocity.y);
+            }
+            else
+            {
+                rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
+            }
+            
+            anim.SetFloat("Speed", Mathf.Abs(rigidbody2d.velocity.x));
+            anim.SetBool("Grounded", isGrounded);
+            */
         }
 
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, GroundCheckRadius, whatIsGround);
+        
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            rigidbody2d.velocity = new Vector2(moveSpeed, rigidbody2d.velocity.y);
-        }
-        else
-        {
-            rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
-        }
-
-
-        anim.SetFloat("Speed", Mathf.Abs(rigidbody2d.velocity.x));
-        anim.SetBool("Grounded", isGrounded);
-
+        
+        /*
         if (healthCurrent != healthMax && !isRegenHealthTrue)
         {
             StartCoroutine(RegainHealth());
@@ -121,4 +121,5 @@ public class PlayerMotor : NetworkBehaviour
         healthCurrent -= 10;
         UpdateHealthBar();
     }
+    */
 }
