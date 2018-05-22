35c3ba703dc5ca32cbd245d4cab98248defe502a
william alfonsson <william.alfonsson@hotmail.com>
2018-05-22 17:33:49 +0200

    Platformar rör sig
    Nu fungerar scriptet som får platformarna att röra sig. Nästa steg blir att göra så om spelaren står på den rörande platformen, så ska spelaren åka med den istället för att glida av som han gör för tillfäligt.

--- PlatformShooter/Assets/MoveingPlatforms.cs
+++ PlatformShooter/Assets/MoveingPlatforms.cs
@@ -4,14 +4,35 @@ using System.Collections;
 public class MoveingPlatforms : MonoBehaviour {
 
     public GameObject platform;
-    
-    // Use this for initialization
-	void Start () {
 
+    public float moveSpeed;
+
+    public Transform currentPoint;
+
+    public Transform[] points;
+
+    public int pointSelection;
+
+ 
+	void Start ()
+    {
+        currentPoint = points[pointSelection];
 	}
 
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+
+        if(platform.transform.position == currentPoint.position)
+        {
+            pointSelection++;
+
+            if(pointSelection == points.Length)
+            {
+                pointSelection = 0;
+            }
+
+            currentPoint = points[pointSelection];
+        }
 	}
 }
