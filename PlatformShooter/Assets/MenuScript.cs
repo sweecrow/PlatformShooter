03567d9bac7bff093080b1f24e9ab5b74b2c241b
william alfonsson <william.alfonsson@hotmail.com>
2018-05-22 18:37:53 +0200

    Menyer
    La till en start meny och en pause meny

---
+++ PlatformShooter/Assets/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public void PlayTheGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitTheGame()
+    {
+        Debug.Log("The game was quit");
+        Application.Quit();
+    }
+}
---
+++ PlatformShooter/Assets/PauseScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
